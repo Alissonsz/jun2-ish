e30bd87c9d68c9d90d19eae9e41885e4413e154e
Alisson Souza <alisson_tecsouza@hotmail.com>
2021-12-18 21:36:46 -0300

    Finishs basic chat layout and add new test to header

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,10 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils';
+import { useRoom } from '../../contexts/roomContext';
 import Header from './header';
+
+jest.mock('../../contexts/roomContext.tsx');
 
 describe('Header component', () => {
   it('renders correctly', () => {
+    const useRoomMocked = mocked(useRoom);
+    const setRoomVideoUrl = jest.fn();
+
+    useRoomMocked.mockReturnValueOnce({
+      setRoomVideoUrl: setRoomVideoUrl,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any);
+
     render(<Header />);
 
     expect(screen.getByText('Jun2-ish')).toBeInTheDocument();
+  });
+
+  it('Updates roomUrl on input submit', () => {
+    const useRoomMocked = mocked(useRoom);
+    const setRoomVideoUrl = jest.fn();
+
+    useRoomMocked.mockReturnValue({
+      setRoomVideoUrl: setRoomVideoUrl,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any);
+
+    render(<Header />);
+
+    const input = screen.getByPlaceholderText('Video url');
+    const button = screen.getByTestId('setRoomUrl');
+
+    fireEvent.change(input, { target: { value: 'www.teste.com' } });
+    fireEvent.click(button);
+
+    expect(setRoomVideoUrl).toHaveBeenCalledWith('www.teste.com');
   });
 });
--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -1,6 +1,6 @@
 import Layout from '../../components/layout';
 import Counter from '../../components/counter';
-import { RoomContext } from '../../contexts/roomContext';
+import { RoomContext, IChatMessage } from '../../contexts/roomContext';
 import { GetServerSideProps } from 'next';
 import { useContext, useEffect, useState } from 'react';
 import VideoPlayer from '../../components/VideoPlayer';
@@ -11,16 +11,18 @@ import Chat from '../../components/Chat';
 interface IRoom {
   name: string;
   videoUrl: string;
+  messages: IChatMessage[];
 }
 
 const Room = (roomInfos: IRoom) => {
-  const { name, videoUrl, setRoomName, setRoomVideoUrl } =
+  const { name, videoUrl, setRoomName, setRoomVideoUrl, setRoomMessages } =
     useContext(RoomContext);
 
   useEffect(() => {
     console.log(roomInfos);
     setRoomVideoUrl(roomInfos.videoUrl);
     setRoomName(roomInfos.name);
+    setRoomMessages(roomInfos.messages);
   }, []);
 
   useEffect(() => {
@@ -29,7 +31,12 @@ const Room = (roomInfos: IRoom) => {
 
   return (
     <Layout home>
-      <section className={classNames('section columns is-fullwidth')}>
+      <section
+        className={classNames(
+          'section columns is-fullwidth',
+          styles['columns-container']
+        )}
+      >
         <div className="column is-9">
           <VideoPlayer />
         </div>
@@ -46,6 +53,17 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     props: {
       name: "Gamer's room",
       videoUrl: 'https://www.youtube.com/watch?v=lrE5CC1up3s',
+      messages: [
+        {
+          author: 'Alisson',
+          content: 'Hello everyone',
+        },
+        {
+          author: 'Iguzinho',
+          content: "It's cabulozo big cruzeiro hahahaah games",
+        },
+        { author: 'GLucaXD', content: "Let's go Bahia" },
+      ],
     },
   };
 };
